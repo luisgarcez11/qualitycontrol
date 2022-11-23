cea8573051053a2e322b2af960bc99e0dfc2192b
luisgarcez11 <luisgarcez1@mail.com>
2022-11-23 21:03:20 +0000

    fix testing directories

--- tests/testthat/test-read_qc_mapping.R
+++ tests/testthat/test-read_qc_mapping.R
@@ -2,24 +2,24 @@
 test_that("dimension/expected errors", {
 
 
-  obj <- qualitycontrol::read_qc_mapping(path = "data-raw/qc_mapping.xlsx")
+  obj <- qualitycontrol::read_qc_mapping(path =  testthat::test_path("test_datasets", "qc_mapping.xlsx"))
 
   testthat::expect_gte(length(obj), 1)
   testthat::expect_true(is.list(obj))
   
   
-  testthat::expect_error(qualitycontrol::read_qc_mapping(path = "data-raw/test_datasets/qc_mapping_error1.xlsx"))
-  testthat::expect_error(qualitycontrol::read_qc_mapping(path = "data-raw/test_datasets/qc_mapping_error2.xlsx"))
-  testthat::expect_error(qualitycontrol::read_qc_mapping(path = "data-raw/test_datasets/qc_mapping_error3.xlsx"))
-  testthat::expect_error(qualitycontrol::read_qc_mapping(path = "data-raw/test_datasets/qc_mapping_error4.xlsx"))
-  testthat::expect_error(qualitycontrol::read_qc_mapping(path = "data-raw/test_datasets/qc_mapping_error5.xlsx"))
-  testthat::expect_error(qualitycontrol::read_qc_mapping(path = "data-raw/test_datasets/qc_mapping_error6.xlsx"))
-  testthat::expect_error(qualitycontrol::read_qc_mapping(path = "data-raw/test_datasets/qc_mapping_error7.xlsx"))
-  testthat::expect_error(qualitycontrol::read_qc_mapping(path = "data-raw/test_datasets/qc_mapping_error8.xlsx"))
-  testthat::expect_error(qualitycontrol::read_qc_mapping(path = "data-raw/test_datasets/qc_mapping_error9.xlsx"))
-  testthat::expect_error(qualitycontrol::read_qc_mapping(path = "data-raw/test_datasets/qc_mapping_error10.xlsx"))
-  testthat::expect_error(qualitycontrol::read_qc_mapping(path = "data-raw/test_datasets/qc_mapping_error11.xlsx"))
-  testthat::expect_error(qualitycontrol::read_qc_mapping(path = "data-raw/test_datasets/qc_mapping_error12.xlsx"))
+  testthat::expect_error(qualitycontrol::read_qc_mapping(path = testthat::test_path("test_datasets", "qc_mapping_error1.xlsx")))
+  testthat::expect_error(qualitycontrol::read_qc_mapping(path = testthat::test_path("test_datasets", "qc_mapping_error2.xlsx")))
+  testthat::expect_error(qualitycontrol::read_qc_mapping(path = testthat::test_path("test_datasets", "qc_mapping_error3.xlsx")))
+  testthat::expect_error(qualitycontrol::read_qc_mapping(path = testthat::test_path("test_datasets", "qc_mapping_error4.xlsx")))
+  testthat::expect_error(qualitycontrol::read_qc_mapping(path = testthat::test_path("test_datasets", "qc_mapping_error5.xlsx")))
+  testthat::expect_error(qualitycontrol::read_qc_mapping(path = testthat::test_path("test_datasets", "qc_mapping_error6.xlsx")))
+  testthat::expect_error(qualitycontrol::read_qc_mapping(path = testthat::test_path("test_datasets", "qc_mapping_error7.xlsx")))
+  testthat::expect_error(qualitycontrol::read_qc_mapping(path = testthat::test_path("test_datasets", "qc_mapping_error8.xlsx")))
+  testthat::expect_error(qualitycontrol::read_qc_mapping(path = testthat::test_path("test_datasets", "qc_mapping_error9.xlsx")))
+  testthat::expect_error(qualitycontrol::read_qc_mapping(path = testthat::test_path("test_datasets", "qc_mapping_error10.xlsx")))
+  testthat::expect_error(qualitycontrol::read_qc_mapping(path = testthat::test_path("test_datasets", "qc_mapping_error11.xlsx")))
+  testthat::expect_error(qualitycontrol::read_qc_mapping(path = testthat::test_path("test_datasets", "qc_mapping_error12.xlsx")))
 
 
 
